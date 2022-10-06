8fbbd8b9e4ef5c92c61df5b1e772b58709b5d227
Richiiz <ricardo.rodriguez@librerialeo.com.mx>
2022-10-06 17:39:35 -0500

    se vio el comando reduce y lo que se realiza con este

---
+++ server/05-reduce.js
@@ -0,0 +1,12 @@
+const totals= [1,2,3,4,5];
+
+let sum = 0;
+for (let index = 0; index < totals.length; index++){
+    const element = totals[index];
+    sum = sum + element;
+}
+console.log(sum);
+
+//lo que hace .reduce es juntar todos los elementos en uno solo elemento
+const rta = totals.reduce((sum, element) => sum + element ,0)
+console.log('rta', rta);
