4261a0e1b3b7e8d5961ac9d2a81157bcd80cd469
Richiiz <ricardo.rodriguez@librerialeo.com.mx>
2022-10-07 17:55:36 -0500

    .split divide un strign en dos o mas subcadenas mientras que .join junta todo con un caracterer que en este caso es '-' por otro lado .tolowercase transforma todo el string en minusculas

---
+++ server/12-join-split.js
@@ -0,0 +1,9 @@
+const elements = ["fire", "air", "water"];
+
+
+const rta = elements.join('--')
+console.log('join', rta);
+
+const title = 'Curso de manipulacion de arrays';
+const titelfinal = title.split(' ').join('-').toLowerCase();
+console.log(titelfinal);
