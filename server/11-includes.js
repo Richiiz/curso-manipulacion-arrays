2b7007ccff125ff163369d8bbdae7a863f5a37a7
Richiiz <ricardo.rodriguez@librerialeo.com.mx>
2022-10-07 17:48:07 -0500

    .includes arroja un false o true en el caso de que no encuentre o si encuentre el objeto, funciona con arrays y strings

---
+++ server/11-includes.js
@@ -0,0 +1,6 @@
+const pets = ['cat', 'dog', 'bat'];
+
+//arroja un false o true en el caso de que no encuentre o si encuentre el objeto, funciona con arrays y strings
+
+const rta = pets.includes('dog');
+console.log('includes', rta)
